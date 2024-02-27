c05e009d1b68862ebdd06763401a26654b2df298
Константин Любушкин <constant-biz@yandex.ru>
2024-02-27 14:25:21 +0300

    Создание админки

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+admin.site.site_header = 'Админ панель'
+admin.site.index_title = 'Администрирование Yatube'
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'author')
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = "-пусто-"
+
+admin.site.register(Post, PostAdmin)
