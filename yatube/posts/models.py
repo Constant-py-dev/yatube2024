bc29590c23018cda91c6e5377c9d93fcad96dc95
Константин Любушкин <constant-biz@yandex.ru>
2024-02-27 16:32:59 +0300

    Создана модель Group и добавлена в админку

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,10 +1,25 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
 
 User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey(Group, blank=True, null=True, on_delete=models.CASCADE, related_name='groups')
