d50e085a1ea07b0ca950515e735854e118539727
Константин Любушкин <constant-biz@yandex.ru>
2024-03-02 11:04:28 +0300

    Установлено приложение django_extensions, создан requirements.txt

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.PostsConfig'
+    'posts.apps.PostsConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
